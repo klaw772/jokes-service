e934abe31713ee50605d41466ff5c0ad58204bbc
Kat Law <kathleen.law@multiverse.io>
2023-03-06 09:14:59 -0500

    init

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Op } = require("sequelize");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
+    const {tags, content} = req.query;
+    let jokes = await Joke.findAll();
+    if (tags) {
+      jokes = await Joke.findAll({
+        where: {tags: {
+          [Op.substring]: tags
+        }}
+      });
+    }
+    else if (content) {
+      jokes = await Joke.findAll({
+        where: {joke: {
+          [Op.substring]: content
+        }}
+      });
+    }
+      res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error)
